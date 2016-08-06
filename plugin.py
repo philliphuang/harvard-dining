3572588d3bee7153acd0575742bae3053248be0d
Phillip Huang <philliphuang@college.harvard.edu>
2016-08-06 07:22:22 -0400

    makes into list

--- plugin.py
+++ plugin.py
@@ -1,23 +1,27 @@
 def results(fields, original_query):
 	
+	# request data from API
 	import requests
-
 	params = {
 		'key': '06bc0ceef35adbd17e8955ad198209c5',
 		'output': 'json',
 		'sdt': '2011-03-21'
 	}
-
 	r = requests.get('http://api.cs50.net/food/3/menus', params=params)
 
+	# format for only names
 	items = []
-
 	for item in r.json():
 		items.append(item['name'])
+		
+	html = "<ul>"
+	for item in items:
+		html = html + "<li>" + item + "</li>"
+	html = html + "</ul>"
 
 	return {
 		"title": "HUDS Menu",
-		"html": "<p style='font-family: sans-serif; padding: 2em'>{0}</p>".format(items)
+		"html": html
 	}
 
 def run(message):
